4017870eb5215c67da1c2288db875e8daaf15116
Admir Ljubovic <admirl@uia.no>
2022-05-03 14:28:34 +0200

    updated firebase config

--- firebase-config.js
+++ firebase-config.js
@@ -8,12 +8,14 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAIDqDNrDHfOtE-w38c7-vJpACEF2AT1IA",
-  authDomain: "final-project-306b6.firebaseapp.com",
-  projectId: "final-project-306b6",
-  storageBucket: "final-project-306b6.appspot.com",
-  messagingSenderId: "977478365445",
-  appId: "1:977478365445:web:a79bf24ae79d2ecc2ec76c",
+  apiKey: "AIzaSyBDIHEPO5AyxT9gbu0ll2eaiRcgF43Z0wg",
+  authDomain: "final-project-54908.firebaseapp.com",
+  databaseURL:
+    "https://final-project-54908-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "final-project-54908",
+  storageBucket: "final-project-54908.appspot.com",
+  messagingSenderId: "839133418558",
+  appId: "1:839133418558:web:50577c6b341ff6271b2d6f",
 };
 
 // Initialize Firebase
