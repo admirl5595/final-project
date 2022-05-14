b2a17f0dae066864ea8ea82f19846cb3c4e99735
Admir Ljubovic <admirl@uia.no>
2022-05-14 22:15:37 +0200

    implemented Header and HeaderAndIcon components

--- src/screens/ComponentsTest/index.js
+++ src/screens/ComponentsTest/index.js
@@ -2,11 +2,15 @@ import { View, Text, StyleSheet } from "react-native";
 import React from "react";
 import { theme } from "src/res/theme";
 import TextInputStyled from "src/components/common/TextInputStyled";
+import Header from "src/components/common/Header";
+import HeaderAndIcon from "src/components/common/HeaderAndIcon";
 
 export default function ComponentsTest() {
   return (
     <View style={styles.container}>
-      <TextInputStyled />
+      <Header title="ComponentsTest" />
+      <HeaderAndIcon icon="gear" iconColor={"grey"} title="Title with icon" />
+      <TextInputStyled placeholder="placeholder" />
     </View>
   );
 }
